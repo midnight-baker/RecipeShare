9e057375d3334719623f06698515a98557d4abea
midnight-baker <124546107+midnight-baker@users.noreply.github.com>
2024-02-28 22:44:52 -0800

    push 1.0.4

---
+++ app/src/main/java/com/example/recipeshare/forgot_password.java
@@ -0,0 +1,13 @@
+package com.example.recipeshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class forgot_password extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}

14eed8c4631127995fde1d35f7d95085d80e71e0
midnight-baker <124546107+midnight-baker@users.noreply.github.com>
2024-02-28 19:43:31 -0800

    push 1.0.2

---
+++ app/src/main/java/com/example/recipeshare/confirm_email.java
@@ -0,0 +1,13 @@
+package com.example.recipeshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class confirm_email extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_email);
+    }
+}
